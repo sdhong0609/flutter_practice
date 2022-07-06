c53d400183fd0bc02a7f87f5d12ee39bd81d5932
sdh041 <sdh041@ladaka.com>
2022-07-06 15:44:12 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/hongstudio/flutter_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hongstudio.flutter_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Character card',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyCard(),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  const MyCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BBANTO'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
