3c2cba8f56ad263d1d87c8b28c625f3a37c3079b
sdh041 <sdh041@ladaka.com>
2022-07-11 14:52:37 +0900

    made SnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Appbar',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
+      title: 'Snack Bar',
+      theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
     );
   }
@@ -24,107 +21,26 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Appbar icon menu'),
-        centerTitle: true,
-        elevation: 0.0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('Shopping cart button is clicked');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('Search button is clicked');
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/bbanto.png'),
-                backgroundColor: Colors.white,
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/cat.jpg'),
-                  backgroundColor: Colors.white,
-                ),
-                // CircleAvatar(
-                //   backgroundImage: AssetImage('assets/cat.jpg'),
-                //   backgroundColor: Colors.white,
-                // ),
-              ],
-              accountName: Text('BBANTO'),
-              accountEmail: Text('bbanto@bbanto.com'),
-              onDetailsPressed: () {
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),
-                  bottomRight: Radius.circular(40.0)
-                )
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: () {
-                print('Home is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('Setting'),
-              onTap: () {
-                print('Setting is clicked');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('Q&A'),
-              onTap: () {
-                print('Q&A is clicked');
-              },
-              trailing: Icon(Icons.add),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Snack Bar'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Builder(
+          builder: (BuildContext ctx) {
+            return Center(
+              child: FlatButton(
+                child: Text(
+                  'Show me',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  Scaffold.of(ctx)
+                      .showSnackBar(SnackBar(content: Text('Hello')));
+                },
+              ),
+            );
+          },
+        ));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
