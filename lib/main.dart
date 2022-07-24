a49fb0d493454698581e0cc12771b0bd51e8cb43
hong <hsd1525@naver.com>
2022-07-24 17:41:44 +0900

    made custom snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -21,22 +24,34 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Snack Bar'),
-          centerTitle: true,
-        ),
-        body: Builder(builder: (ctx) {
-          return Center(
-            child: FlatButton(
-              child: Text('Show me', style: TextStyle(color: Colors.white)),
-              color: Colors.red,
-              onPressed: () {
-                Scaffold.of(ctx).showSnackBar(SnackBar(
-                  content: Text('Hello'),
-                ));
-              },
-            ),
-          );
-        }));
+      appBar: AppBar(
+        title: Text('Snack Bar'),
+        centerTitle: true,
+      ),
+      body: MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+          child: Text('Show me'),
+          onPressed: () {
+            Scaffold.of(context).showSnackBar(SnackBar(
+              content: Text(
+                'Hello',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.teal,
+              duration: Duration(milliseconds: 1000),
+            ));
+          }),
+    );
   }
 }
