2d93f2b237910d4be4202fbd7025d7a5e8ad3565
hong <hsd1525@naver.com>
2022-07-28 00:11:18 +0900

    GetX practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/controller.dart';
+import 'package:get/get.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('GetX'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed: () {}, child: Text('Add number'))
-          ],
-        ),
-      ),
-    );
+    // Controller controller = Get.put(Controller());
+
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('GetX'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GetBuilder<Controller>(
+                  init: Controller(),
+                  builder: (_) =>
+                      Text('Current value is: ${Get.find<Controller>().x}',
+                          style: TextStyle(fontSize: 20, color: Colors.red)),
+                ),
+                ElevatedButton(onPressed: () {
+                  Get.find<Controller>().increment();
+                }, child: Text('Add number'))
+              ],
+            ),
+          ),
+        ));
   }
 }
