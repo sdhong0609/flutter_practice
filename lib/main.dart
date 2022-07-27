3c8df7e866f1a9fe7a309adeb3045e63e72545d2
hong <hsd1525@naver.com>
2022-07-27 16:49:01 +0900

    initial commit on flutter_GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/ScreenA.dart';
-import 'package:flutter_practice/ScreenB.dart';
-import 'package:flutter_practice/ScreenC.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,13 +7,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/' : (context) => ScreenA(),
-        '/b' : (context) => ScreenB(),
-        '/c' : (context) => ScreenC()
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Add number')),
+          ],
+        ),
+      ),
     );
   }
 }
