3dc7165e23ec178e7ddfaf93a8b10f05f87b44f5
hong <sdhong0609@gmail.com>
2022-08-01 00:17:57 +0900

    remove flutter toast

--- lib/main.dart
+++ lib/main.dart
@@ -5,121 +5,32 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyButtons(),
-    );
-  }
-}
-
-class MyButtons extends StatelessWidget {
-  const MyButtons({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Buttons'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {},
-              onLongPress: () {
-                print('text button');
-              },
-              child: Text('Text button', style: TextStyle(fontSize: 20.0)),
-              style: TextButton.styleFrom(
-                primary: Colors.red,
-                // backgroundColor: Colors.blue
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('Elevated button');
-              },
-              child: Text('Elevated button'),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.orangeAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  elevation: 0.0),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print('Outlined button');
-              },
-              child: Text('Outlined button'),
-              style: OutlinedButton.styleFrom(
-                primary: Colors.green,
-                // side: BorderSide(
-                //   color: Colors.black87,
-                //   width: 2.0
-                // )
-              ),
-            ),
-            TextButton.icon(
-              onPressed: () {
-                print('Icon button');
-              },
-              icon: Icon(
-                Icons.home,
-                size: 30.0,
-                // color: Colors.black87,
-              ),
-              label: Text('Go to home'),
-              style: TextButton.styleFrom(primary: Colors.purple),
-            ),
-            ElevatedButton.icon(
-              onPressed: () {
-                print('Elevated Icon button');
-              },
-              icon: Icon(
-                Icons.home,
-                size: 20,
-              ),
-              label: Text('Go to home'),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.black, minimumSize: Size(200, 50)),
-            ),
-            OutlinedButton.icon(
-              onPressed: () {
-                print('Outlined Icon button');
-              },
-              icon: Icon(Icons.home),
-              label: Text('Go to home'),
-              style: OutlinedButton.styleFrom(primary: Colors.black),
-            ),
-            ElevatedButton.icon(
-              onPressed: null,
-              icon: Icon(
-                Icons.home,
-                size: 20,
-              ),
-              label: Text('Go to Home'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.black,
-                onSurface: Colors.pink,
-                // minimumSize: Size(200, 50)
-              ),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              buttonPadding: EdgeInsets.all(20),
-              children: [
-                TextButton(onPressed: () {}, child: Text("TextButton")),
-                ElevatedButton(onPressed: () {}, child: Text("ElevatedButton"))
-              ],
-            )
-          ],
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('You have pushed the button this many times'),
+              Text(
+                '$counter',
+                style: Theme.of(context).textTheme.displaySmall,
+              )
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              counter++;
+              print("$counter");
+            }),
       ),
     );
   }
