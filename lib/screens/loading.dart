14e3df41b518177e9c672919138b52e75e41040a
hong <sdhong0609@gmail.com>
2022-08-09 19:42:02 +0900

    Make weather app part 2

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -9,12 +11,39 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
 
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+  void getLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print('There was a problem with the internet connection');
+    }
+  }
+
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      print(wind);
+
+      var id = jsonDecode(jsonData)['id'];
+      print(id);
+    }
   }
 
   @override
@@ -22,9 +51,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(color: Colors.white),
