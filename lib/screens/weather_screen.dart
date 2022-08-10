a21f4735e57802660f20cd4c15e02cce4d7aef7b
hong <sdhong0609@gmail.com>
2022-08-10 16:23:07 +0900

    Make weather app part 4

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData});
@@ -32,25 +34,53 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        // title: Text(''),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.near_me),
+          onPressed: () {},
+          iconSize: 30.0,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_searching),
+            onPressed: () {},
+            iconSize: 30.0,
+          )
+        ],
+      ),
+      body: Container(
+        child: Stack(
           children: [
-            Text(
-              '$cityName',
-              style: TextStyle(fontSize: 30.0),
+            Image.asset(
+              'image/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              '$temp',
-              style: TextStyle(fontSize: 30.0),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 150.0,
+                  ),
+                  Text(
+                    'Seoul',
+                    style: GoogleFonts.lato(
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  )
+                ],
+              ),
             )
           ],
         ),
-      )),
+      ),
     );
   }
 }
