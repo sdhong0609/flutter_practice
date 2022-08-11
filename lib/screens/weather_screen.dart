007d6a393a53d18ddf1d837b8e394012ff35986a
hong <sdhong0609@gmail.com>
2022-08-11 18:18:21 +0900

    Make weather app part 4

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData});
@@ -14,6 +16,7 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   String cityName = "";
   int temp = 0;
+  var date = DateTime.now();
 
   @override
   void initState() {
@@ -29,6 +32,11 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
     print(temp);
     print(cityName);
+  }
+
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat("h:mm a").format(now);
   }
 
   @override
@@ -64,16 +72,172 @@ class _WeatherScreenState extends State<WeatherScreen> {
             Container(
               padding: EdgeInsets.all(20.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SizedBox(
-                    height: 150.0,
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 150.0,
+                            ),
+                            Text(
+                              'Seoul',
+                              style: GoogleFonts.lato(
+                                  fontSize: 35.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(Duration(minutes: 1),
+                                    builder: (context) {
+                                  print('${getSystemTime()}');
+                                  return Text(
+                                    '${getSystemTime()}',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 16.0,
+                                      color: Colors.white,
+                                    ),
+                                  );
+                                }),
+                                Text(DateFormat(' - EEEE, ').format(date),
+                                    style: GoogleFonts.lato(
+                                      fontSize: 16.0,
+                                      color: Colors.white,
+                                    )),
+                                Text(DateFormat('d MMM, yyy').format(date),
+                                    style: GoogleFonts.lato(
+                                      fontSize: 16.0,
+                                      color: Colors.white,
+                                    ))
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '18\u2103',
+                              style: GoogleFonts.lato(
+                                  fontSize: 85.0,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset('svg/climacon-sun.svg'),
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text(
+                                  'clear sky',
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0, color: Colors.white),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                  Text(
-                    'Seoul',
-                    style: GoogleFonts.lato(
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
+                  Column(
+                    children: [
+                      Divider(
+                        height: 15.0,
+                        thickness: 2.0,
+                        color: Colors.white30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'AQI(대기질지수)',
+                                style: GoogleFonts.lato(
+                                    fontSize: 16.0, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Image.asset(
+                                'image/bad.png',
+                                width: 37.0,
+                                height: 35.0,
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '"매우나쁨"',
+                                style: GoogleFonts.lato(
+                                    fontSize: 16.0, color: Colors.black87,
+                                fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '미세먼지',
+                                style: GoogleFonts.lato(
+                                    fontSize: 16.0, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '174.75',
+                                style: GoogleFonts.lato(
+                                    fontSize: 26.0, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '㎍/m³',
+                                style: GoogleFonts.lato(
+                                    fontSize: 16.0, color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '초미세먼지',
+                                style: GoogleFonts.lato(
+                                    fontSize: 16.0, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '84.03',
+                                style: GoogleFonts.lato(
+                                    fontSize: 26.0, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '㎍/m³',
+                                style: GoogleFonts.lato(
+                                    fontSize: 16.0, color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
                   )
                 ],
               ),
