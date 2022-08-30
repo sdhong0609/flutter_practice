434dd6adab7aca25e7888e2eaf5ac6143c227ccd
hong <sdhong0609@gmail.com>
2022-08-30 23:01:55 +0900

    Make chat app (sending message)

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/chatting/chat/message.dart';
+import 'package:flutter_practice/chatting/chat/new_message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -49,31 +50,15 @@ class _ChatScreenState extends State<ChatScreen> {
             )
           ],
         ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('chats/JwLdkTSNwU4XMXtkjDtI/message')
-              .snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final docs = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: docs.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    docs[index]['text'],
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                );
-              },
-            );
-          },
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: Messages(),
+              ),
+              NewMessage(),
+            ],
+          ),
         ));
   }
 }
